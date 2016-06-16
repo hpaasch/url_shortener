fcba664f14c88a22d4453abeca3b7cc58f8f136a
hpaasch <hepaasch@gmail.com>
2016-06-16 15:50:29 -0400

    User setup partially done.

--- shortener_app/views.py
+++ shortener_app/views.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView, CreateView
 
 
 class IndexView(View):
-    pass
+    # template_name = 'index_view.html'
+    #
+    # def get_context_data(self, **kwargs):
+    #     context['name'] = 'hi hope'
+    #     return context
+    def get(self, request):
+        return HttpResponse('Welcome to URL shortener')
+
+class RegisterView(CreateView):  # creates a user
+    model = User
+    form_class = UserCreationForm
+    # success_url = "/"  # back to homepage/index view
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,11 +1,15 @@
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 from shortener_app import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.IndexView.as_view(), name='index_view'),
+    url(r'^$', views.IndexView.as_view(), name='index_view'),  # needs to show something
+    url(r'^register_view/$', views.RegisterView.as_view(), name='register_view'),
+    url(r'^login_view/$', login, name='login_view'),
+    url(r'^logout_view/$', logout, name='logout_view'),
 ]
