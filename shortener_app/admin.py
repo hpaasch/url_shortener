56d6831c8e1dc8b8947ee1fc32a435c06db83933
hpaasch <hepaasch@gmail.com>
2016-06-16 20:33:04 -0400

    URL table created.

--- shortener_app/admin.py
+++ shortener_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from shortener_app.models import Click
+
+admin.site.register(Click)
--- shortener_app/models.py
+++ shortener_app/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Click(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=60)
+    description = models.TextField(blank=True)  # makes this optional
+    created = models.DateTimeField(auto_now_add=True)
+    url_user = models.ForeignKey(User)
+    short_code = models.CharField(max_length=25)
+    private = models.NullBooleanField()
+
+    def __str__(self):
+        return self.title
+
